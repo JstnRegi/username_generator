f7564c9fd161a5c79a9e8bdfcb676e5f1ed3753d
Justin Reyes <Justin@Justins-MacBook-Pro.local>
2015-09-15 17:01:44 -0700

    username_generator finished

--- username.rb
+++ username.rb
@@ -5,20 +5,44 @@ def format_name(first, last)
   if last == '' || first == ''
     word = nil
   else
-  word = []
-    user_name = word.push(first.gsub(' ', '')[0])
-    word.push(last)
-    user_name = word.join('').gsub(/[^A-Za-z]/, '')
-    user_name.downcase
+  user_name = ''
+    user_name += first.gsub(' ', '').gsub(/[^0-9A-Za-z]/, '')[0]
+    user_name += (last.gsub(' ','').gsub(/[^0-9A-Za-z]/, ''))
+    user_name.gsub(/\d/, '').downcase
   end
 end
 
-def format_year
+def format_year(str)
+  if str.to_s.length != 4
   nil
+  else
+  str.to_s.slice(2,2)
+  end
 end
 
-def build_username
-  nil
+def build_username(first, last, year, usertype = 0)
+  if usertype == 1
+    username = 'seller-'
+  elsif usertype == 2
+    username = 'manager-'
+  elsif usertype == 3
+    username = 'admin-'
+  else
+  username = ''
+  end
+
+      user_name = first.gsub(' ', '').gsub(/[^0-9A-Za-z]/, '')[0]
+      user_name += (last.gsub(' ','').gsub(/[^0-9A-Za-z]/, ''))
+      username += user_name.gsub(/\d/, '').downcase
+
+    if year.to_s.length != 4
+      nil
+    else
+      username += year.to_s.slice(2,2)
+    end
+
+  username
+
 end
 
 
